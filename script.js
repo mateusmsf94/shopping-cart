e2128febce5e24699015259c517fb0c364d74c26
Mateus M. S. A. Figueiredo <mateusmelosf@gmail.com>
2022-09-13 13:49:53 -0300

    requisito 5

--- script.js
+++ script.js
@@ -30,6 +30,10 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
+const cartItemClickListener = (e) => {
+  e.target.remove()
+}
+ 
 /**
  * Função responsável por criar e retornar um item do carrinho.
  * @param {Object} product - Objeto do produto.
@@ -42,7 +46,7 @@ const createCartItemElement = ({ id, title, price }) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `ID: ${id} | TITLE: ${title} | PRICE: $${price}`;
-  // li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener);
   return li;
 };
 
